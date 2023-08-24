27292310312370935198c72ba5783831b66aa52b
prius2055 <prius2055@gmail.com>
2023-08-24 02:10:44 +0100

    Fix linters

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,19 +1,19 @@
 class Api::V1::GreetingsController < ApplicationController
-    # before_action :set_greeting
+  # before_action :set_greeting
 
-    def index
-        @greetings = Greeting.all
-        render json: @greetings
-    end
+  def index
+    @greetings = Greeting.all
+    render json: @greetings
+  end
 
-    def show
-        @greeting = Greeting.find(params[:id])
-        # render json: @greeting
-    end
+  def show
+    @greeting = Greeting.find(params[:id])
+    # render json: @greeting
+  end
 
-    private
+  private
 
-    def set_greeting
-        @greeting = Greeting.find(params[:id])
-    end
+  def set_greeting
+    @greeting = Greeting.find(params[:id])
+  end
 end
